f0416364b01d3b962879fbea5590c78d5766a3d0
pedrocpacheco <cpedropacheco@hotmail.com>
2023-07-07 11:47:54 -0300

    feat(Section-02-1-Printing): strings

---
+++ Section-02/1-Printando/strings.js
@@ -0,0 +1,10 @@
+// Prints Strings:
+
+// "" -> Aspas Duplas 
+console.log("'Pedrinho!'")
+
+// '' -> Aspas Simples
+console.log('"Pedro!"')
+
+// `` -> Crases (pode ter "" e '' dentro alem de ${})
+console.log(`"Pedro" e 'Pedro'`)
