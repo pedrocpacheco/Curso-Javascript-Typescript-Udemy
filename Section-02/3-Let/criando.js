d19620019027da89b945f7c33f389a75ddd5cfcd
pedrocpacheco <cpedropacheco@hotmail.com>
2023-07-07 12:22:52 -0300

    feat: learing let variables

---
+++ Section-02/3-Let/criando.js
@@ -0,0 +1,14 @@
+// Salvando Nome
+let nome = 'Pedro' // Salva uma String em memoria
+console.log(nome) // Printa a String salvada -> Pedro
+
+// Salvando Numero
+nome = 10         // Não há tipagem
+console.log(nome) // Printa o Numero salvo -> 10
+
+// Não inicalizando
+let naoDeclarada // Apenas declarou
+console.log(naoDeclarada) // Não da erro -> undefined
+
+naoDeclarada = "Declarada"
+console.log(naoDeclarada) // Printa Declarada
---
+++ Section-02/3-Let/padroes.js
@@ -0,0 +1,25 @@
+// 1- Não podemos criar variaveis com palavras reservadas
+//let if
+
+// 2- Variaveis precisam ter nomes significativos
+let n = "Pedro"
+console.log(n)
+// Funciona, mas é muito ruim.
+
+// 3- Não pode começar variavel com numero
+//let 1nome 
+let no1me // Mas pode no meio, ou no fim
+
+// 4- Padrão de palavras compostas -> camelCase
+let nomeCompleto = "Pedro Pacheco"
+console.log(nomeCompleto)
+
+// 5- Case Sensitive -> Variaveis diferentes por causa de case
+let nomeCliente = "Pedro"
+let nomecliente = "pedro"
+
+// 6- Não podemos redeclarar variavies com let
+let declarada = "declarada"
+let naoDeclarada = "não declarada"
+
+// Não utilize VAR, utilize LET!
