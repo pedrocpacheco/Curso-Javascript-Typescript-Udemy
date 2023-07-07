ef9c76ded6c954ea80e59d7ed4395fd754fe8076
pedrocpacheco <cpedropacheco@hotmail.com>
2023-07-07 18:22:51 -0300

    feat(arrays): creating arrays and acessing data

---
+++ Section-02/10-Arrays/1-criando.js
@@ -0,0 +1,9 @@
+// Nesse arquivo, vamos ver o basico de Arrays
+
+// 1- Criando Array de apenas um tipo de dado
+const students = ['Pedro', 'Luane', 'Daniel']
+console.log(students);
+
+// 2- Criando Array com varios tipos de dados
+const diverse = ['Pedro', 'Luane', 1, 2, true, false, null, undefined]
+console.log(diverse);
---
+++ Section-02/10-Arrays/2-acessando.js
@@ -0,0 +1,13 @@
+// Neste arquivo, vamos descobrir como acessar os itens de um Array
+
+// 1- Acessando o valor de um index do array
+const students = ['Pedro', 'Luane', 'Daniel']
+console.log(students[0]); // Pedro
+console.log(students[1]); // Luane
+console.log(students[2]); // Daniel
+// Ele acessa o valor no index do que foi passado no []
+
+// 2- Salvando um valor de um array em uma variavel
+const stu1 = students[0] // Pedro
+const stu2 = students[1] // Luane
+const stu3 = students[2] // Daniel
