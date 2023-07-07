54adab1544702a2583da2e3eb095d32a36a58937
pedrocpacheco <cpedropacheco@hotmail.com>
2023-07-07 11:50:10 -0300

    feat: printing numbers

---
+++ Section-02/1-Printando/numbers.js
@@ -0,0 +1,11 @@
+// Printando Numeros
+
+console.log(123456) // Numeros Inteiros
+
+console.log(9.99) // Numeros Quebrados
+
+console.log(10, 9, "Pedro") // Varias coisas no mesmo Console.log
+
+// Não existe diferença de um numero inteiro para um quebrado
+
+// Todo numero em Javascript é Number (não há float/int)
