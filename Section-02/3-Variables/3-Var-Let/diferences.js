1b3c364429ef3091be724b0a2268910b3c76015a
pedrocpacheco <cpedropacheco@hotmail.com>
2023-07-07 13:03:23 -0300

    feat: differences between var and let

---
+++ Section-02/3-Variables/3-Var-Let/diferences.js
@@ -0,0 +1,14 @@
+// 1- Redeclarando variavies
+
+// Criando var
+var nomeVar = 'Pedro'
+console.log(nomeVar) // Pedro
+var nomeVar = 'Thiago' // Funciona
+
+// Criando let
+let nomeLet = 'Pedro'
+console.log(nomeLet)
+// let nomeLet = "Thiago" // Da Erro
+
+// 2- Variavel Global
+nome = 'Pedro' // NÃO FAÇA ISSO -> cria var global
--- Section-02/3-Variables/exer/exer.js
+++ Section-02/3-Variables/exer/exer.js
@@ -1,3 +1,9 @@
+/*
+Pedro Pacheco tem 30 anos, pesa 84 kg
+tem 1.7 de altura e seu IMC é de 20
+Pedro Pacheco nasceu em 1980
+*/
+
 const firstName = "Pedro"
 const lastName = "Pacheco"
 const age = 17
@@ -9,6 +15,6 @@ let birthDate
 imc = weightKg / (heigthM * 2)
 birthDate = 2023 - age
 
-console.log(`${firstName} ${lastName} tem ${age} anos, e pesa ${peso}`)
+console.log(`${firstName} ${lastName} tem ${age} anos, e pesa ${weightKg}`)
 console.log(`tem ${heigthM} e seu IMC é de ${imc}`)
-console.log(`${firstName} ${lastName} nasceu em ${birthDate}`)
+console.log(`${firstName} nasceu em ${birthDate}`)
