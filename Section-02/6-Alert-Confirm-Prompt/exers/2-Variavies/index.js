cf9b5262ef40b7f71ef2b91c4856f7eaebba744a
pedrocpacheco <cpedropacheco@hotmail.com>
2023-07-07 15:53:42 -0300

    feat: calculator and variable exers

---
+++ Section-02/6-Alert-Confirm-Prompt/exers/2-Variavies/index.js
@@ -0,0 +1,20 @@
+// Neste exercicio, precisamos transformar o valor dar variaveis
+// Sem ser com o valor bruto, e sim com as das outras proprias variaveis
+
+// Variaveis no Começo
+let varA = 'A'; // B
+let varB = 'B'; // C
+let varC = 'C'; // A
+
+// Primeira Opção
+const varATemp = varA
+varA = varB
+varB = varC
+varC = varATemp
+
+console.log(varA, varB, varC);
+
+// Segunda Opção
+[varA, varB, varC] = [varB, varC, varA]
+
+console.log(varA, varB, varC);
