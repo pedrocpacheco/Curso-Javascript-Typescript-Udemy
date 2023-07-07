deec55a823f67b64a3c59547e7c8235617666e75
pedrocpacheco <cpedropacheco@hotmail.com>
2023-07-07 12:56:49 -0300

    feat: let and const exercise

---
+++ Section-02/4-Const/exer/exer.js
@@ -0,0 +1,14 @@
+const firstName = "Pedro"
+const lastName = "Pacheco"
+const age = 17
+const weightKg = 70 
+const heigthM = 1.75
+let imc
+let birthDate
+
+imc = weightKg / (heigthM * 2)
+birthDate = 2023 - age
+
+console.log(`${firstName} ${lastName} tem ${age} anos, e pesa ${peso}`)
+console.log(`tem ${heigthM} e seu IMC é de ${imc}`)
+console.log(`${firstName} ${lastName} nasceu em ${birthDate}`)
