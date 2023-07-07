f64de590d14c593eb28e5a845d2035840c29953a
pedrocpacheco <cpedropacheco@hotmail.com>
2023-07-07 12:32:56 -0300

    feat: const

---
+++ Section-02/4-Const/criando.js
@@ -0,0 +1,15 @@
+// Salvando Nome
+const nome = "Pedro" // Sempre se inicia a constante
+console.log(nome)
+// const nome = "Pacheco" // Não se modifica o valor da constante
+
+// Associando uma cosntante a outra
+const num1 = 10
+const num2 = num1
+
+const result = num1 * num2
+const doubleResult = result * 2
+
+console.log(doubleResult)
+// Os valores aqui nunca se modificam
+// Podendo assim ser reutilizados
