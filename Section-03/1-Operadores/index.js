d70631d1c13d75d7bceddca99b4bd2206c09d889
pedrocpacheco <cpedropacheco@hotmail.com>
2023-07-18 13:08:19 -0300

    feat: comparison operators

---
+++ Section-03/1-Operadores/index.js
@@ -0,0 +1,27 @@
+/*
+Operadores de Comparação:
+> maior que
+>= maior ou igual 
+< menor que
+<= menor ou igual
+== igualdade (valor) "10" == 10
+=== igualdade (valor e tipo) 10 === 10
+!= diferente (valor) "10" != 9
+!== diferente (valor e tipo) "10" !== 10
+*/
+
+const comp = 10 > 5 // boolean
+console.log(comp) // true
+
+console.log(10 > 5); // false
+console.log(10 >= 10) // true
+
+console.log(10 < 5); // false
+console.log(5 <= 5); // true
+
+console.log(10 == "10"); // true
+console.log(10 === "10"); // false
+
+console.log(10 != "10"); // false
+console.log(10 !== "10"); // true
+
