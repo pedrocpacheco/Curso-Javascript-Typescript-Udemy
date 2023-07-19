e971470f210155bbf429d38e2779a33cd7894e1c
pedrocpacheco <cpedropacheco@hotmail.com>
2023-07-19 07:26:33 -0300

    feat: var let & const differences part2

---
+++ Section-03/7-Let-Var-Const/index.js
@@ -0,0 +1,45 @@
+const verdadeira = true
+
+// Let tem escopo de bloco {... bloco}
+// Var só tem escopo de função
+
+// 1- Escopo de Bloco 
+
+let nome = 'Pedro'
+// let nome = 'Pacheco' -> Da erro, não pode (pois ja tem uma no bloco)
+
+var nome2 = 'Thiago'
+// var nome2 = 'Fritz' -> Funciona, o nome2 é redeclarado (globalmente)
+
+console.log(nome, nome2);
+
+if(verdadeira){
+    let nome = 'Pacheco' // pode -> criando outra variavel neste novo bloco
+    var nome2 = 'Dante' // Redeclarada
+    console.log(nome, nome2);
+    if(verdadeira){
+        let  nome = 'Carvalho' // pode -> criando outra variavel nesse novo bloco
+        var nome2 = 'Aliglieri' // Redeclarada
+        console.log(nome, nome2);
+    }
+}
+
+console.log(nome, nome2); // let = Pedro | var = Aliglieri
+// Ou seja, quando redeclaramos um let, é só no escopo, o var é global
+
+// 2- Escopo de Função
+
+function sayName(){
+    var l = 'L'
+    console.log(l);
+}
+// console.log(l); -> Não pode fazer isso, L so existe na função
+// variaveis dentro de uma função, só existem dentro dela
+
+var sobrenome = 'Lawiet'
+function sayLastName() {
+    console.log(sobrenome);
+}
+
+console.log(sobrenome);
+// variaveis fora de uma função, pode ser usadas dentro dela
