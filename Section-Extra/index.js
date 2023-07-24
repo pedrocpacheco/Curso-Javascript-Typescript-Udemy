cf3cfa8ff0270f0f5cb454c953d659c5f99e4ca7
pedrocpacheco <cpedropacheco@hotmail.com>
2023-07-24 18:09:55 -0300

    feat: nodemon and first .get route

--- Section-Extra/index.js
+++ Section-Extra/index.js
@@ -2,4 +2,8 @@ const express = require("express");
 
 const app = express();
 
+app.get("/test", (request, response) => {
+    response.send("Tudo Certo!")
+})
+
 app.listen(3000, () => console.log("Rodando"));
