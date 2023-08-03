d7b581a73a91d836186a4d6b50a2a295d95a4dcf
pedrocpacheco <cpedropacheco@hotmail.com>
2023-08-03 09:38:24 -0300

    feat(nic-jong): post method for new post

--- Section-Blog/nic-jong/src/index.js
+++ Section-Blog/nic-jong/src/index.js
@@ -1,12 +1,30 @@
-const express = require("express")
+const express = require("express");
 
 const app = express();
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 
 app.use(expres.json());
 
 app.get("/", (req, res) => {
     res.send("Hello World!")
+});
+
+const posts = [
+    {id: 1, title: "Post 1"},
+    {id: 2, title: "Post 2"},
+    {id: 3, title: "Post 3"}
+];
+
+app.get("/posts", (req, res) => {
+    res.json(posts);
+})
+
+app.post("/posts", (req, res) =>{
+    const newPost = {
+        id: posts.length + 1,
+        title: req.body.title
+    }
+    res.status(201).json(newPost);
 })
 
 app.listen(PORT, () => {
